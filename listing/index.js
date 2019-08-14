6c69a08c2ed60e57929086627c83ac99e1f47d08
Giacomo Maria Cerreto <giacomo@soluzionifutura.it>
2019-08-14 12:03:50 +0200

    listing wip

--- listing/index.js
+++ listing/index.js
@@ -1,46 +1,72 @@
-'use strict'
+"use strict"
 
-const fetch = require('node-fetch')
+const fetch = require("node-fetch")
+
+// sort by price, date, discount, name
+// discriminant can be lang, category, product status, quantity
+/*const list = (offset, limit, sort, discriminant) => {
+  const body = {
+    "from": offset,
+    "size": limit,
+    "sort": [],
+    //"query": {},
+    "_source": {
+        "includes": [],
+        "excludes": []
+    }
+  }
+}*/
+
 
 const body = {
   "from" : 0, "size" : 10,
-//  "sort": [
-//      {"productBasePrice": "asc"}
-//  ]
+  "sort": [ {
+      "productBasePrice": "asc",
+      "productSalePrice": {
+        "order": "asc",
+        "missing": "_last",
+        
+          "filter": {
+            "range": {
+              "productSalePrice": {
+                "gte": 0
+              }
+            }
+          }
+        
+      },
+      "productSpecialPrice": {
+        "order": "asc",
+        "missing": "_last",
+          "filter": {
+            "range": {
+              "productSpecialPrice": {
+                "gte": 0
+              }
+            }
+          }
+        
+      }
+    }  
+  ]
 }
  
-fetch('http://localhost:9200/holyproducts/_search', {
-    method: 'post',
-    body:    JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
-  })
-  .then(res => res.json())
-  .then(json => {
-    const hits = json.hits.hits
-        
-    const data = hits.map(hit => hit._source)
+fetch("http://localhost:9200/holyproducts/_search", {
+  method: "post",
+  body:    JSON.stringify(body),
+  headers: { "Content-Type": "application/json" },
+})
+.then(res => res.json())
+.then(json => {
+  const hits = json.hits.hits
+  const data = hits.map(hit => hit._source)
 
-    return {
-      skip: body.from,
-      limit: body.size,
-      took: json.took,
-      count: json.hits.total,
-      products: data
-    }
-  })
-  .then(json => {
-    const sorted = json.products.sort((product1, product2) => {
-      const min1 = Math.min(product1.productBasePrice, product1.productSpecialPrice, product1.productSalePrice)
-      const min2 = Math.min(product2.productBasePrice, product2.productSpecialPrice, product2.productSalePrice)
-      if (min1 <= min2) {return -1} else {return 1}
-    })
-
-    return {
-      skip: json.skip,
-      limit: json.limit,
-      took: json.took,
-      count: json.count,
-      products: sorted
-    }
-  })
-  .then(json => console.log(JSON.stringify(json, null, 2)))
+  return {
+    skip: body.from,
+    limit: body.size,
+    took: json.took,
+    count: json.hits.total,
+    products: data
+  }
+})
+.then(json => console.log(JSON.stringify(json, null, 2)))
